c4ed38e9359ed4dbc18653ad9cdd37a0f11c50b3
Anika Malhotra <you@example.com>
2024-07-03 22:53:45 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "IRS",
+    version = '0.0.0',
+    author = 'Anika',
+    author_email = 'malhotraanika07@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
